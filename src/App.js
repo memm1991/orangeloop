8e75b6207224347ebcb3058ae6f384ba005c6c9d
memm1991 <72989632+memm1991@users.noreply.github.com>
2021-07-26 10:54:32 -0300

    Dark mode Challenge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import{useState} from 'react'
+import moon from './img/moon.png'
+import sun from './img/sun.png'
 
 function App() {
+  const [mode,setmode]=useState(false)
+  function handlermode (){
+    setmode(!mode)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={mode?'darkmode':'lightmode'}>
+    <h2 className='titulo'>Dark Mode Challenge</h2>
+    <div className='imagen'>
+      {mode?<img src={moon} alt=''/>:<img src={sun} alt=''/>}
     </div>
+    
+    <div className='parrafos'>
+    <p className='parra1'>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <p className='parra1'>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+
+    </div>
+     <div className='inputs'>
+    <input
+    className='inp'
+    type='text'
+    placeholder='Name'/>
+     <input
+     className='inp'
+    type='text'
+    placeholder='Email'/>
+    </div>
+    <div className='buttons'>
+    <button className='exit' onClick={(e)=>handlermode()}>Exit</button>
+    <button className='save' onClick={(e)=>handlermode()}>Save</button>
+    </div>
+
+    
+</div>
   );
 }
 
